3bf84662e2dea6e86da0d806e09651437c4825ff
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2025-04-08 15:32:55 -0300

    finalizar programa com menu interativo
    adiciona a funcionalidade de cada uma das opções e corrige um erro de formato na leitura da opção

--- menu-interativo.c
+++ menu-interativo.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+void verificacaoImparOuPar()
+{
+    int numero;
+
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    if (numero % 2 == 0)
+    {
+        printf("Esse número é par\n");
+    }
+    else
+    {
+        printf("Esse número é impar\n");
+    }
+}
+
+void imprimirNumeros()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+
 int main()
 {
     int opcoes;
@@ -10,8 +35,9 @@ int main()
         printf("Escolha uma das opções de 1 a 3:\n");
         printf(" 1 > Calcular se um número é ímpar ou par\n");
         printf(" 2 > Imprimir os números de 1 a 10\n");
-        printf(" 3 > Sair.");
-        scanf("d%", &opcoes);
+        printf(" 3 > Sair.\n");
+        printf("Faça sua escolha: ");
+        scanf("%d", &opcoes);
         switch (opcoes)
         {
         case 1:
@@ -21,7 +47,7 @@ int main()
             imprimirNumeros();
             break;
         case 3:
-            print("Saindo do programa...");
+            printf("Saindo do programa...\n");
             return 0;
         default:
             printf("Opção inválida. Tente novamente utilizando as opções válidas de 1 a 3.\n");
